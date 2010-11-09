badf6b8ba77a8baf2ae9cdf697e01e81f6c0d793
Ronny Dudeck <rdu@internetschmie.de>
2010-11-09 11:37:26 +0100

    first heartbeat

--- app/src/main/java/de/guxx/ttdroid/app/ttdoidAppActivity.java
+++ app/src/main/java/de/guxx/ttdroid/app/ttdoidAppActivity.java
@@ -26,6 +26,11 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.Socket;
 import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -37,6 +42,7 @@ import java.util.logging.Logger;
  */
 public class ttdoidAppActivity extends Activity
 {
+
     protected BluetoothAdapter bluetoothAdapter;
 
     /**
@@ -46,184 +52,249 @@ public class ttdoidAppActivity extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        testBluetooth();
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+	testBluetooth();
     }
 
     private void testBluetooth()
     {
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null)
-        {
-            // Device does not support Bluetooth
-        }
+	bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	if (bluetoothAdapter == null)
+	{
+	    // Device does not support Bluetooth
+	}
 
-        if (!bluetoothAdapter.isEnabled())
-        {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 1);
-        }
+	if (!bluetoothAdapter.isEnabled())
+	{
+	    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	    startActivityForResult(enableBtIntent, 1);
+	}
 
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0)
-        {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices)
-            {
-                // Add the name and address to an array adapter to show in a ListView
-                System.out.println(device.getName() + "\n" + device.getAddress());
-//                Thread t = new ConnectThread(device);
-//                t.start();
-                Thread s = new AcceptThread();
-                s.start();
-            }
-        }
-    }
-
-    private class AcceptThread extends Thread
-    {
-        private final BluetoothServerSocket mmServerSocket;
-
-        public AcceptThread()
-        {
-            // Use a temporary object that is later assigned to mmServerSocket,
-            // because mmServerSocket is final
-            BluetoothServerSocket tmp = null;
-            try
-            {
-                // MY_UUID is the app's UUID string, also used by the client code
-                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord("test", UUID.randomUUID());
-            } catch (IOException e)
-            {
-            }
-            mmServerSocket = tmp;
-        }
-
-        @Override
-        public void run()
-        {
-            BluetoothSocket socket = null;
-            // Keep listening until exception occurs or a socket is returned
-            System.out.println("start");
-            while (true)
-            {
-                try
-                {
-                    socket = mmServerSocket.accept();
-                    System.out.println("socket");
-                } catch (IOException e)
-                {
-                    System.out.println("exception");
-                    break;
-                }
-                // If a connection was accepted
-                if (socket != null)
-                {
-                    // Do work to manage the connection (in a separate thread)
-                    manageConnectedSocket(socket);
-                    try
-                    {
-                        mmServerSocket.close();
-                    } catch (IOException ex)
-                    {
-                        System.out.println("problem close");
-                    }
-                    break;
-                }
-            }
-        }
-
-        /** Will cancel the listening socket, and cause the thread to finish */
-        public void cancel()
-        {
-            try
-            {
-                System.out.println("close");
-                mmServerSocket.close();
-            } catch (IOException e)
-            {
-            }
-        }
-
-        private void manageConnectedSocket(BluetoothSocket socket)
-        {
-            System.out.println("manage");
-        }
+	Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+	// If there are paired devices
+	if (pairedDevices.size() > 0)
+	{
+	    // Loop through paired devices
+	    for (BluetoothDevice device : pairedDevices)
+	    {
+		// Add the name and address to an array adapter to show in a ListView
+		System.out.println(device.getName() + "\n" + device.getAddress());
+		Thread t = new ConnectThread(device);
+		t.start();
+//                Thread s = new AcceptThread();
+//                s.start();
+	    }
+	}
     }
 
     private class ConnectThread extends Thread
     {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
 
-        public ConnectThread(BluetoothDevice device)
-        {
-            // Use a temporary object that is later assigned to mmSocket,
-            // because mmSocket is final
-            BluetoothSocket tmp = null;
-            mmDevice = device;
+	private final BluetoothSocket mmSocket;
+	private final BluetoothDevice mmDevice;
 
-            // Get a BluetoothSocket to connect with the given BluetoothDevice
-            try
-            {
-                // MY_UUID is the app's UUID string, also used by the server code
-                tmp = device.createRfcommSocketToServiceRecord(UUID.randomUUID());
-            } catch (IOException e)
-            {
-            }
-            mmSocket = tmp;
-        }
+	public ConnectThread(BluetoothDevice device)
+	{
+	    // Use a temporary object that is later assigned to mmSocket,
+	    // because mmSocket is final
+	    BluetoothSocket tmp = null;
+	    mmDevice = device;
 
-        @Override
-        public void run()
-        {
-            // Cancel discovery because it will slow down the connection
+	    // Get a BluetoothSocket to connect with the given BluetoothDevice
+	    try
+	    {
+		// MY_UUID is the app's UUID string, also used by the server code
+		System.out.println("preSocket");
+		tmp = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+		System.out.println("postSocket");
+	    }
+	    catch (IOException e)
+	    {
+	    }
+	    mmSocket = tmp;
+	}
+
+	@Override
+	public void run()
+	{
+	    // Cancel discovery because it will slow down the connection
 //            bluetoothAdapter.cancelDiscovery();
-            System.out.println("discovery");
+	    System.out.println("socket:" + mmSocket);
 
-            try
-            {
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
-                mmSocket.connect();
-                System.out.println("connect");
-            } catch (IOException connectException)
-            {
-                // Unable to connect; close the socket and get out
-                try
-                {
-                    System.out.println("unable to connect");
-                    mmSocket.close();
-                } catch (IOException closeException)
-                {
-                    System.out.println("close exception");
-                }
-                return;
-            }
+	    try
+	    {
+		// Connect the device through the socket. This will block
+		// until it succeeds or throws an exception
+		mmSocket.connect();
+		System.out.println("connect");
+	    }
+	    catch (IOException connectException)
+	    {
+		// Unable to connect; close the socket and get out
+		try
+		{
+		    System.out.println("unable to connect");
+		    mmSocket.close();
+		}
+		catch (IOException closeException)
+		{
+		    System.out.println("close exception");
+		}
+		return;
+	    }
 
-            // Do work to manage the connection (in a separate thread)
-            manageConnectedSocket(mmSocket);
-        }
+	    // Do work to manage the connection (in a separate thread)
+	    manageConnectedSocket(mmSocket);
+	}
 
-        /** Will cancel an in-progress connection, and close the socket */
-        public void cancel()
-        {
-            try
-            {
-                System.out.println("close");
-                mmSocket.close();
+	/** Will cancel an in-progress connection, and close the socket */
+	public void cancel()
+	{
+	    try
+	    {
+		System.out.println("close");
+		mmSocket.close();
 
-            } catch (IOException e)
-            {
-            }
-        }
+	    }
+	    catch (IOException e)
+	    {
+	    }
+	}
 
-        private void manageConnectedSocket(BluetoothSocket mmSocket)
-        {
-            System.out.println(mmSocket);
-            System.out.println("manage");
-        }
+	private void manageConnectedSocket(BluetoothSocket socket)
+	{
+	    System.out.println(mmSocket);
+	    System.out.println("manage");
+	    try
+	    {
+		InputStream in = socket.getInputStream();
+		OutputStream out = socket.getOutputStream();
+
+		HxmReader hr = new HxmReader(in);
+
+		while (true)
+		{
+		    HxmPaket paket = hr.read();
+		    paket.validate();
+		    System.out.println(paket);
+		}
+	    }
+	    catch (Exception ex)
+	    {
+		System.out.println("io exception");
+	    }
+	}
+
+	public class HxmPaket
+	{
+
+	    private final static int checksumPolynomial = 0x8c;
+	    private final static byte STX = 0x02;
+	    private final static byte ETX = 0x03;
+	    private final static byte HXM_ID = 0x26;
+	    private final static byte HXM_DLC = 0x37;
+	    private byte[] data;
+	    private int dataSize;
+
+	    public HxmPaket(byte[] data, int dataSize)
+	    {
+		this.data = data;
+		this.dataSize = dataSize;
+	    }
+
+	    @Override
+	    public String toString()
+	    {
+		return "hxmPaket: len: " + dataSize + "\nheart: " + data[12] + "\nbeat: " + data[13];
+	    }
+
+	    public boolean validate()
+	    {
+		if (dataSize != 60)
+		{
+		    System.out.println("error: datasize");
+		    return false;
+		}
+
+		if (data[0] != STX)
+		{
+		    System.out.println("error STX");
+		    return false;
+		}
+
+		if (data[1] != HXM_ID)
+		{
+		    System.out.println("error HXM_ID");
+		    return false;
+		}
+
+		if (data[2] != HXM_DLC)
+		{
+		    System.out.println("error HXM_DLC");
+		    return false;
+		}
+
+		if (data[59] != ETX)
+		{
+		    System.out.println("error ETX");
+		    return false;
+		}
+
+		if (!isCRC())
+		{
+		    System.out.println("error CRC");
+		    return false;
+		}
+
+		return true;
+	    }
+
+	    private boolean isCRC()
+	    {
+		int crc = 0;
+
+		for (int i = 2; i < 57; i++)
+		{
+		    crc = crcPushByte(crc, ((int)data[i] &0xff));
+		}
+		System.out.println("crc: " + crc + " == " + ((int)data[57] &0xff));
+		return (crc == ((int)data[57] &0xff));
+	    }
+
+	    private int crcPushByte(int crc, int b)
+	    {
+		crc = (crc ^ b);
+		
+		for (int t = 0; t < 8; t++)
+		{
+		    if ((crc & 1) == 1) crc = (( crc >> 1) ^ checksumPolynomial);
+		    else crc = (crc >> 1);
+		}
+		return crc;
+	    }
+	}
+
+	private class HxmReader
+	{
+
+	    public InputStream stream;
+
+	    public HxmReader(InputStream stream)
+	    {
+		this.stream = stream;
+	    }
+
+	    public HxmPaket read() throws IOException
+	    {
+		byte[] array = new byte[60];
+		int size = stream.read(array);
+		while (size < 60)
+		{
+		    size += stream.read(array, size, 60 - size);
+		}
+		return new HxmPaket(array, size);
+	    }
+	}
     }
 }
