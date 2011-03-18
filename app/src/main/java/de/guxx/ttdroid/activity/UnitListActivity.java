a6e45af711ed869a5863ea2ead04a91160946c0f
Ronny Dudeck <rdu@internetschmie.de>
2011-03-18 15:19:04 +0100

    new activity

---
+++ app/src/main/java/de/guxx/ttdroid/activity/UnitListActivity.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2010-2010 rdu <ich@ronny-dudeck.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program at http://www.gnu.org/licenses/licence/gpl_v3.txt.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.guxx.ttdroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import de.guxx.ttdroid.view.UnitListView;
+
+public class UnitListActivity extends Activity
+{    
+    private UnitListView view;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        view = new UnitListView(this);
+        setContentView(view);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Runtime.getRuntime().exit(0);
+    }    
+}
---
+++ app/src/main/java/de/guxx/ttdroid/view/UnitListView.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2010-2010 rdu <ich@ronny-dudeck.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program at http://www.gnu.org/licenses/licence/gpl_v3.txt.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.guxx.ttdroid.view;
+
+import android.content.Context;
+import android.view.View;
+
+public class UnitListView extends View
+{
+    public UnitListView(Context context)
+    {
+        super(context);
+    }    
+}
